bc4f5ab076730cd62402901a71785ab1f9dffa74
louie_gomez <kinggomez027.lg@gmail.com>
2022-08-19 14:55:56 -0400

    Task deliverable

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function Task() {
+function Task({ task }) {
+  console.log(task)
+
+  function handleClick(event) {
+    event.target.parentElement.remove()
+  }
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{task.category}</div>
+      <div className="text">{task.text}</div>
+      <button onClick={handleClick} className="delete">X</button>
     </div>
   );
 }
