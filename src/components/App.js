400e0fef139b104bd8348654986ca11b0059e5c1
louie_gomez <kinggomez027.lg@gmail.com>
2022-08-19 17:03:08 -0400

    categoryfilter deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -9,17 +9,33 @@ console.log({ CATEGORIES, TASKS });
 
 function App() {
   const [tasks, setTasks] = useState(TASKS)
+  const [filter, setFilter] = useState("All")
 
   function handleDelete(text) {
     const filteredTasks = tasks.filter(task => task.text !== text)
     setTasks(filteredTasks)
   }
+
+  const handleClick = (e) => {
+    setFilter(e.target.innerText)
+  }
+
+  const displayTasks = () => {
+    return tasks.filter(task => {
+      if(filter === "All") {
+        return true
+      } else {
+        return task.category === filter
+      }
+    })
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter categories={CATEGORIES} />
+      <CategoryFilter handleClick={handleClick} categories={CATEGORIES} />
       <NewTaskForm />
-      <TaskList tasks={tasks} handleDelete={handleDelete} />
+      <TaskList tasks={displayTasks()} handleDelete={handleDelete} />
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
 
-function CategoryFilter({ categories }) {
+function CategoryFilter({ categories, handleClick }) {
   const renderButtons = () => {
     return categories.map(category => {
-      return <button key={uuidv4()}>{category}</button>
+      return <button onClick={handleClick} key={uuidv4()}>{category}</button>
     })
   }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
