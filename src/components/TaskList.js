725aed23c8f50ebff7cc1980997985ff67cf8397
louie_gomez <kinggomez027.lg@gmail.com>
2022-08-19 14:26:55 -0400

    TaskList deliverable

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks }) {
+  const displayTasks = () => {
+    return tasks.map((task) => {
+      return <Task task={task} />
+    })
+  }
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {displayTasks()}
     </div>
   );
 }
