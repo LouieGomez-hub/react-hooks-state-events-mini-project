2d9f5bfc5c9e8709d8be41e0f379e41fea18f14c
louie_gomez <kinggomez027.lg@gmail.com>
2022-08-19 14:30:32 -0400

    TaskList deliverables

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Task from "./Task";
+import { v4 as uuidv4 } from "uuid";
 
 function TaskList({ tasks }) {
   const displayTasks = () => {
     return tasks.map((task) => {
-      return <Task task={task} />
+      return <Task key={uuidv4()} task={task} />
     })
   }
   return (
